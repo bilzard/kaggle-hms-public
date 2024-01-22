e3a26592de238826d1ea1b37f5de7c591634ac18
bilzard <nakamine11@gmail.com>
2024-01-22 11:52:22 +0900

    labelのpreprocess処理を削除

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,7 +5,7 @@ import torchaudio.functional as AF
 from einops import rearrange
 
 from src.array_util import pad_multiple_of
-from src.constant import EEG_PROBES, LABELS, PROBES
+from src.constant import EEG_PROBES, PROBES
 
 
 def load_eeg(eeg_id, data_dir, phase="train"):
@@ -37,19 +37,6 @@ def process_mask(eeg: pl.DataFrame, down_sampling_rate=5) -> np.ndarray:
     )
     mask = mask[down_sampling_rate // 2 :: down_sampling_rate, :]
     return mask.to_numpy()
-
-
-def quality_estimator(x: np.ndarray, T: float = 30) -> np.ndarray:
-    assert T > 0.0
-    return np.log1p(x.clip(0, T)) / np.log1p(T)
-
-
-def process_label(metadata: pl.DataFrame, T: float = 30):
-    labels = metadata.select(f"{label}_vote" for label in LABELS).to_numpy()
-    total_votes = labels.sum(axis=1, keepdims=True)
-    labels = labels / total_votes
-    quality = quality_estimator(total_votes, T)
-    return labels, quality, total_votes
 
 
 def do_apply_filter(
