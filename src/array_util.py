15e34a2167048171ed17d59ac99bcd69753f97c4
bilzard <nakamine11@gmail.com>
2024-01-25 16:47:38 +0900

    pad_multiple_ofを任意の次元のarrayを指定できるようにする

--- src/array_util.py
+++ src/array_util.py
@@ -9,17 +9,25 @@ def find_nearest_multiple(n, m):
     return n + (m - n % m) % m
 
 
-def pad_multiple_of(xs: np.ndarray, divisor: int, pad_value=0) -> np.ndarray:
+def pad_multiple_of(
+    xs: np.ndarray, divisor: int, axis: int = 0, pad_value=0
+) -> np.ndarray:
     """Pad a array with zeros so that its length is a multiple of divisor.
+    padding is applied to the first dimension of the array.
 
     Args:
-        xs: A 1-D array.
-        divisor: The divisor of the length of the tensor.
-        pad_value: The value to pad the tensor with.
+        xs: numpy array of N-dimensions
+        divisor: The divisor of the length of the tensor
+        axis: The axis to apply padding (default=0)
+        pad_value: The value to pad the tensor with (default=0)
 
     Returns:
         The padded array.
     """
-    length = find_nearest_multiple(xs.shape[0], divisor)
-    pad_size = length - xs.shape[0]
-    return np.pad(xs, [[0, pad_size]], constant_values=pad_value)
+    length = find_nearest_multiple(xs.shape[axis], divisor)
+    pad_size = length - xs.shape[axis]
+    return np.pad(
+        xs,
+        [(0, pad_size) if i == axis else (0, 0) for i in range(xs.ndim)],
+        constant_values=pad_value,
+    )
