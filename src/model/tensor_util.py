96d407943765762c304b484727a854fa13287008
bilzard <nakamine11@gmail.com>
2024-02-01 19:55:21 +0900

    tensor関連のhelper functionを実装
    - rolling_mean
    - same_padding_1d

---
+++ src/model/tensor_util.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+def same_padding_1d(
+    x: torch.Tensor, kernel_size: int, stride: int, **kwargs
+) -> torch.Tensor:
+    pad_size = kernel_size - stride
+    pad_left = pad_size // 2
+    pad_right = pad_size - pad_left
+    return nn.functional.pad(x, (pad_left, pad_right), **kwargs)
+
+
+def rolling_mean(x: torch.Tensor, kernel_size: int, stride: int) -> torch.Tensor:
+    """
+    x: (B, C, T)
+    """
+    B, C, T = x.shape
+    pad_size = (kernel_size - stride) // 2
+    conv = nn.Conv1d(
+        in_channels=C,
+        out_channels=C,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=pad_size,
+        bias=False,
+        groups=C,
+    )
+    conv = conv.to(x.device)
+    conv.weight.data.fill_(1.0 / kernel_size)
+    result = conv(x)
+    return result
