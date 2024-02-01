62f8e3064cf9f6eb78a61e2abdbf03f0eb4051f9
bilzard <nakamine11@gmail.com>
2024-02-01 20:26:40 +0900

    wave2specの改良
    - ヘルパー関数を使って書き換え
    - downsample方式を引数で切り替えられるようにする

--- src/model/feature_extractor/wave2spec.py
+++ src/model/feature_extractor/wave2spec.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torchaudio.functional as AF
 from torchaudio.transforms import MelSpectrogram
 
 from src.constant import PROBE2IDX
+from src.model.tensor_util import rolling_mean, same_padding_1d
 
 
 class Wave2Spectrogram(nn.Module):
@@ -25,6 +25,8 @@ class Wave2Spectrogram(nn.Module):
         db_offset=0,
         n_mels=128,
         window_fn=torch.hann_window,
+        apply_mask=True,
+        downsample_mode="linear",
     ):
         super().__init__()
         self.n_fft = n_fft
@@ -38,6 +40,8 @@ class Wave2Spectrogram(nn.Module):
         self.db_cutoff = db_cutoff
         self.db_offset = db_offset
         self.sampling_rate = sampling_rate
+        self.apply_mask = apply_mask
+        self.downsample_mode = downsample_mode
 
         self.wave2spec = MelSpectrogram(
             sample_rate=sampling_rate,
@@ -51,25 +55,39 @@ class Wave2Spectrogram(nn.Module):
             window_fn=window_fn,
         )
 
-    def same_padding_waveform(self, signals: torch.Tensor, value=0, mode="reflect"):
-        """
-        STFTの出力系列長が「入力系列長//hop_length」となるようにpaddingする
-        """
-        pad_size = int((self.n_fft - self.hop_length) / 2)
-        signals = F.pad(signals, (pad_size, pad_size, 0, 0), mode=mode, value=value)
-        return signals
-
-    def downsample(self, x: torch.Tensor):
+    def downsample_mask(self, x: torch.Tensor, mode="nearest") -> torch.Tensor:
         """
         1次元信号をダウンサンプリングする
         """
-        x = x[..., self.n_fft // 2 :: self.hop_length]
+        x = same_padding_1d(
+            x,
+            kernel_size=self.n_fft,
+            stride=self.hop_length,
+            mode="replicate",
+        )
+        if mode == "nearest":
+            x = x[..., self.n_fft // 2 :: self.hop_length]
+        elif mode == "linear":
+            x = rolling_mean(
+                x,
+                kernel_size=self.n_fft,
+                stride=self.hop_length,
+                apply_padding=False,
+            )
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
+
         return x
 
     @torch.no_grad
-    def forward(
-        self, x: torch.Tensor, mask: torch.Tensor | None = None, apply_mask=True
-    ):
+    def forward(self, x: torch.Tensor, mask: torch.Tensor | None = None):
+        """
+        return:
+        - signal: (B, C, T)
+        - spectrogram: (B, C, F, T)
+        - channel_mask: (B, C, T)
+        - spec_mask: (B, C, 1, T)
+        """
         _, num_frames, _ = x.shape
         spectrograms = []
         signals = []
@@ -95,7 +113,7 @@ class Wave2Spectrogram(nn.Module):
             channel_mask = []
             for p1, p2 in zip(probes[:-1], probes[1:]):
                 x_diff = x[..., PROBE2IDX[p1]] - x[..., PROBE2IDX[p2]]
-                if apply_mask:
+                if self.apply_mask:
                     x_diff *= mask[..., PROBE2IDX[p1]] * mask[..., PROBE2IDX[p2]]
                 signal.append(x_diff)
                 channel_mask.append(mask[..., PROBE2IDX[p1]] * mask[..., PROBE2IDX[p2]])
@@ -112,13 +130,12 @@ class Wave2Spectrogram(nn.Module):
                     signal, self.sampling_rate, self.cutoff_freqs[1]
                 )
 
-            spec_mask = self.same_padding_waveform(
-                channel_mask, mode="constant", value=0
-            )
-            spec_mask = self.downsample(spec_mask)
+            spec_mask = self.downsample_mask(channel_mask, mode=self.downsample_mode)
             spec_mask = spec_mask.unsqueeze(dim=2)
 
-            spec = self.same_padding_waveform(signal)
+            spec = same_padding_1d(
+                signal, kernel_size=self.n_fft, stride=self.hop_length, mode="reflect"
+            )
             spec = self.wave2spec(spec)
             spec = (
                 AF.amplitude_to_DB(
