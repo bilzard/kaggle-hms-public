20c3819b45756b9d40b291bc87892c0df3a68496
bilzard <nakamine11@gmail.com>
2024-02-08 15:07:47 +0900

    W&Bにmetricsを送る

--- src/callback/metrics_logger.py
+++ src/callback/metrics_logger.py
@@ -1,12 +1,18 @@
 import torch
+import wandb
 
 from src.callback.base import Callback
 
 
 class MetricsLogger(Callback):
+    def __init__(self):
+        self.wandb_enabled = wandb.run is not None
+
     @torch.no_grad()
     def on_train_epoch_end(self, trainer, epoch: int, loss: float):
         print(f"[epoch {epoch}] train_loss: {loss:.4f}")
+        if self.wandb_enabled:
+            wandb.log({"epoch/train_loss": loss, "epoch": epoch})
 
     @torch.no_grad()
     def on_train_step_end(
@@ -16,11 +22,19 @@ class MetricsLogger(Callback):
         output: dict[str, torch.Tensor],
         loss: float,
     ):
-        pass
+        if self.wandb_enabled:
+            wandb.log(
+                {
+                    f"step/lr{i}": lr
+                    for i, lr in enumerate(trainer.scheduler.get_last_lr())
+                }
+            )
 
     @torch.no_grad()
     def on_valid_epoch_end(self, trainer, epoch: int, loss: float):
         print(f"[epoch {epoch}] validation_loss: {loss:.4f}")
+        if self.wandb_enabled:
+            wandb.log({"epoch/val_loss": loss, "epoch": epoch})
 
     @torch.no_grad()
     def on_valid_step_end(
