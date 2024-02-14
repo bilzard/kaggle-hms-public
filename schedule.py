22984eae7f60dda8b06266571a20cc2a432d15e3
bilzard <nakamine11@gmail.com>
2024-02-14 16:23:37 +0900

    scheduleスクリプトでjob_nameを選べるようにする

--- schedule.py
+++ schedule.py
@@ -16,15 +16,16 @@ def parse_multi_vars(ctx, param, value):
 
 
 @click.command()
+@click.argument("job_name")
 @click.option("--exp_names", callback=parse_multi_vars, default="exp001")
 @click.option("--folds", callback=parse_multi_vars, default="0 1 2 3 4")
 @click.option("--seeds", callback=parse_multi_vars, default="42")
 @click.option("--dry_run", is_flag=True)
-def run_experiments(exp_names, folds, seeds, dry_run):
+def run_experiments(job_name, exp_names, folds, seeds, dry_run):
     for exp_name in exp_names:
         for fold in folds:
             for seed in seeds:
-                cmd = f"python -m run.train --config-name={exp_name} job_name=train fold={fold} seed={seed}"
+                cmd = f"python -m run.{job_name} --config-name={exp_name} job_name={job_name} fold={fold} seed={seed}"
                 print(cmd)
                 if not dry_run:
                     subprocess.run(cmd, shell=True)
