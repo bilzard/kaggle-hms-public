c36b4dd43eb304066ae23d33a7f9e993b39f3f14
bilzard <nakamine11@gmail.com>
2024-02-15 19:16:46 +0900

    ラベルのドメイン知識を反映したheadを実装

---
+++ src/model/head/label_aligned_head.py
@@ -0,0 +1,115 @@
+import torch
+import torch.nn as nn
+from einops import rearrange
+
+from src.model.model_util import GeMPool2d, InverseSoftmax
+
+
+class LabelAlignedHead(nn.Module):
+    """
+    labelに関するドメイン知識を反映したヘッド
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        hidden_channels: int = 256,
+        drop_rate: float = 0.0,
+        eps: float = 1e-4,
+        C: float = 3.0,
+    ):
+        super().__init__()
+        self.feat = nn.Conv2d(
+            in_channels, hidden_channels, kernel_size=1, stride=1, padding=0, bias=True
+        )
+        self.laterality = nn.Conv2d(
+            hidden_channels, 1, kernel_size=1, stride=1, padding=0, bias=True
+        )  # 0: general, 1: lateral
+        self.seizure_type = nn.Conv2d(
+            hidden_channels, 4, kernel_size=1, stride=1, padding=0, bias=True
+        )  # 0: seizure, 1: PD, 2: RDA, 3: others
+        self.pool = GeMPool2d()
+        self.drop_rate = drop_rate
+        self.dropout = nn.Dropout2d(drop_rate, inplace=True)
+
+        self.inverse_softmax = InverseSoftmax(dim=1, eps=eps, C=C)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        x: (batch_size, in_channels, h, w)
+        return: (batch_size, out_channels)
+        """
+        if self.drop_rate > 0:
+            x = self.dropout(x)
+        x = self.feat(x)
+        x = self.pool(x)
+
+        laterality = self.laterality(x).sigmoid().squeeze(dim=1)
+        seizure_type = self.seizure_type(x).softmax(dim=1)
+
+        p_seizure = seizure_type[:, 0]
+        p_lpd = seizure_type[:, 1] * laterality
+        p_gpd = seizure_type[:, 1] * (1 - laterality)
+        p_lrda = seizure_type[:, 2] * laterality
+        p_grda = seizure_type[:, 2] * (1 - laterality)
+        p_other = seizure_type[:, 3]
+
+        x = torch.stack([p_seizure, p_lpd, p_gpd, p_lrda, p_grda, p_other], dim=1)
+        x = self.inverse_softmax(x)
+
+        x = rearrange(x, "b c 1 1 -> b c")
+
+        return x
+
+
+class LabelAlignedHeadV2(nn.Module):
+    """
+    labelに関するドメイン知識を反映したヘッド
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        hidden_channels: int = 256,
+        drop_rate: float = 0.0,
+        eps: float = 1e-4,
+        C: float = 3.0,
+    ):
+        super().__init__()
+        self.feat = nn.Conv2d(
+            in_channels, hidden_channels, kernel_size=1, stride=1, padding=0, bias=True
+        )
+        self.laterality = nn.Conv2d(
+            hidden_channels, 1, kernel_size=1, stride=1, padding=0, bias=True
+        )  # -: general, +: lateral
+        self.seizure_type = nn.Conv2d(
+            hidden_channels, 4, kernel_size=1, stride=1, padding=0, bias=True
+        )  # 0: seizure, 1: PD, 2: RDA, 3: others
+        self.pool = GeMPool2d()
+        self.drop_rate = drop_rate
+        self.dropout = nn.Dropout2d(drop_rate, inplace=True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        x: (batch_size, in_channels, h, w)
+        return: (batch_size, out_channels)
+        """
+        if self.drop_rate > 0:
+            x = self.dropout(x)
+        x = self.feat(x)
+        x = self.pool(x)
+
+        laterality = self.laterality(x).squeeze(dim=1)
+        seizure_type = self.seizure_type(x)
+
+        p_seizure = seizure_type[:, 0]
+        p_lpd = seizure_type[:, 1] + laterality
+        p_gpd = seizure_type[:, 1] - laterality
+        p_lrda = seizure_type[:, 2] + laterality
+        p_grda = seizure_type[:, 2] - laterality
+        p_other = seizure_type[:, 3]
+
+        x = torch.stack([p_seizure, p_lpd, p_gpd, p_lrda, p_grda, p_other], dim=1)
+        x = rearrange(x, "b c 1 1 -> b c")
+
+        return x
--- src/model/model_util.py
+++ src/model/model_util.py
@@ -17,3 +17,14 @@ class GeMPool2d(nn.Module):
         return F.adaptive_avg_pool2d(x.clamp(min=self.eps).pow(self.p), (1, 1)).pow(
             1.0 / self.p
         )
+
+
+class InverseSoftmax(nn.Module):
+    def __init__(self, dim: int, eps: float = 1e-4, C: float = 0.0):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.C = nn.Parameter(torch.ones(1) * C)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.log(x + self.eps) * self.C
