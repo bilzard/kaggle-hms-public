bbb86fdad7ba6755edc17db5f54a53015e3bdb06
bilzard <nakamine11@gmail.com>
2024-02-15 19:16:46 +0900

    ラベルのドメイン知識を反映したheadを実装

--- src/model/head/__init__.py
+++ src/model/head/__init__.py
@@ -1,3 +1,4 @@
+from .label_aligned_head import LabelAlignedHead, LabelAlignedHeadV2
 from .simple_head import SimpleHead
 
-__all__ = ["SimpleHead"]
+__all__ = ["SimpleHead", "LabelAlignedHead", "LabelAlignedHeadV2"]
