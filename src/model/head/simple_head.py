ca8d2aadd24c2aeb7eb4761f7b65b4c7c4f3790e
bilzard <nakamine11@gmail.com>
2024-02-15 17:25:47 +0900

    headにchannel dropoutを追加

--- src/model/head/simple_head.py
+++ src/model/head/simple_head.py
@@ -6,18 +6,22 @@ from src.model.model_util import GeMPool2d
 
 
 class SimpleHead(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self, in_channels: int, out_channels: int, drop_rate: float = 0.0):
         super().__init__()
         self.conv = nn.Conv2d(
             in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=True
         )
         self.pool = GeMPool2d()
+        self.drop_rate = drop_rate
+        self.dropout = nn.Dropout2d(drop_rate, inplace=True)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         x: (batch_size, in_channels, h, w)
         return: (batch_size, out_channels)
         """
+        if self.drop_rate > 0:
+            x = self.dropout(x)
         x = self.conv(x)
         x = self.pool(x)
         x = rearrange(x, "b c 1 1 -> b c")
