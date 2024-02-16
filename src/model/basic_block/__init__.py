fa3b39a7739e5c4de72b08cfa89758ba0944bea8
bilzard <nakamine11@gmail.com>
2024-02-16 21:36:29 +0900

    model_utilをbasic blockに移動

--- src/model/basic_block/__init__.py
+++ src/model/basic_block/__init__.py
@@ -1,4 +1,11 @@
 from .gated_attention import GatedMilAttention, GatedSpecAttention
 from .gem import GeMPool1d, GeMPool2d
+from .inverse_softmax import InverseSoftmax
 
-__all__ = ["GatedMilAttention", "GatedSpecAttention", "GeMPool1d", "GeMPool2d"]
+__all__ = [
+    "GatedMilAttention",
+    "GatedSpecAttention",
+    "GeMPool1d",
+    "GeMPool2d",
+    "InverseSoftmax",
+]
