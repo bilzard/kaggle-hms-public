602ce14764c6f5d1a6ad95e3b668cddbe6f26035
bilzard <nakamine11@gmail.com>
2024-02-21 19:48:17 +0900

    ensembleスクリプト実行時に分析結果を出力する
    - loss distribution
    - total prob/conditional prob matrix
    - eeg_id, loss pair

--- run/ensemble.py
+++ run/ensemble.py
@@ -8,6 +8,7 @@ from torch.nn.functional import kl_div, log_softmax
 
 from src.config import EnsembleMainConfig
 from src.constant import LABELS
+from src.error_analysis import plot_confusion_matrix, plot_loss_distribution
 from src.infer_util import load_metadata, make_submission
 from src.proc_util import trace
 
@@ -53,6 +54,7 @@ def main(cfg: EnsembleMainConfig):
                     phase=cfg.phase,
                     fold_split_dir=fold_split_dir,
                     group_by_eeg=True,
+                    weight_key="weight_per_eeg",
                 )
                 eval_df = metadata.join(predictions, on="eeg_id")
 
@@ -65,24 +67,27 @@ def main(cfg: EnsembleMainConfig):
                 gts = torch.from_numpy(gts.copy())
                 preds = torch.from_numpy(preds.copy())
                 weights = torch.from_numpy(weights.copy())
+                eeg_ids = eval_df["eeg_id"].to_list()
 
                 with torch.no_grad():
+                    losses = kl_div(
+                        log_softmax(preds, dim=1),
+                        gts,
+                        reduction="none",
+                    )
                     loss_per_label = (
-                        (
-                            (
-                                kl_div(
-                                    log_softmax(preds, dim=1),
-                                    gts,
-                                    reduction="none",
-                                )
-                                * weights[:, np.newaxis]
-                            ).sum(dim=0)
-                            / weights.sum()
-                        )
+                        ((losses * weights[:, np.newaxis]).sum(dim=0) / weights.sum())
                         .detach()
                         .cpu()
                         .numpy()
                     )
+
+                # for error analysis
+                losses = losses.detach().cpu().numpy()
+                preds = preds.detach().cpu().numpy()
+                gts = gts.detach().cpu().numpy()
+                weights = weights.detach().cpu().numpy()
+
                 loss = loss_per_label.sum()
                 loss_per_label = dict(zip(LABELS, loss_per_label.tolist()))
                 print(f"loss: {loss:.4f}")
@@ -90,6 +95,39 @@ def main(cfg: EnsembleMainConfig):
                     "loss_per_label:",
                     ", ".join([f"{k}={v:.4f}" for k, v in loss_per_label.items()]),
                 )
+                print("losses:", losses.shape)
+                np.save("losses.npy", losses.astype(np.float16))
+                np.save("eeg_ids.npy", eeg_ids)
+                np.save("gts.npy", gts.astype(np.float16))
+                np.save("preds.npy", preds.astype(np.float16))
+                np.save("weights.npy", weights.astype(np.float16))
+
+                figure_path = Path("figure")
+                if not figure_path.exists():
+                    figure_path.mkdir()
+
+                plot_loss_distribution(
+                    preds,
+                    gts,
+                    weights,
+                    save_file=True,
+                    apply_softmax=True,
+                    root_path=figure_path,
+                )
+                plot_confusion_matrix(
+                    preds,
+                    gts,
+                    weights,
+                    save_file=True,
+                    apply_softmax=True,
+                    root_path=figure_path,
+                )
+                losses = losses.sum(axis=1)
+
+                pl.DataFrame(dict(eeg_id=eeg_ids, loss=losses)).write_parquet(
+                    "losses.pqt"
+                )
+
             case "test":
                 submission_df = make_submission(
                     eeg_ids=predictions["eeg_id"].to_list(),
