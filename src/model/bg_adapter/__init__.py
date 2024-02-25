db63cdf5fbe80b7cbab245ff7a1ba2a9239a6769
bilzard <nakamine11@gmail.com>
2024-02-25 18:52:41 +0900

    implement bg padding transform

--- src/model/bg_adapter/__init__.py
+++ src/model/bg_adapter/__init__.py
@@ -1,5 +1,10 @@
 from .aggregate import BgDualStackAggregator
 from .normalize import BgConstantNormalizer
-from .transform import BgResizeTransform
+from .transform import BgPaddingTransform, BgResizeTransform
 
-__all__ = ["BgDualStackAggregator", "BgConstantNormalizer", "BgResizeTransform"]
+__all__ = [
+    "BgDualStackAggregator",
+    "BgConstantNormalizer",
+    "BgResizeTransform",
+    "BgPaddingTransform",
+]
--- src/model/bg_adapter/transform.py
+++ src/model/bg_adapter/transform.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 from torch import Tensor
 
 
@@ -24,4 +25,23 @@ class BgResizeTransform(nn.Module):
 
     def forward(self, spec: Tensor) -> Tensor:
         spec = self.resize(spec)
+        return spec
+
+
+class BgPaddingTransform(nn.Module):
+    def __init__(
+        self,
+        size: tuple[int, int],
+    ):
+        super().__init__()
+        self.size = tuple(size)
+
+    def forward(self, spec: Tensor) -> Tensor:
+        _, _, f, t = spec.shape
+        assert (
+            f <= self.size[0] and t <= self.size[1]
+        ), f"spec size must be smaller than {self.size}"
+        pad_size = (0, self.size[1] - t, 0, self.size[0] - f)
+        spec = F.pad(spec, pad_size, value=0)
+
         return spec
