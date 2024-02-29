fb4c30a19dd05cb05af81cc770e94dfd4e49ce32
bilzard <nakamine11@gmail.com>
2024-02-29 16:24:30 +0900

    背景specのnormalization adapterを実装

--- src/model/bg_adapter/normalize.py
+++ src/model/bg_adapter/normalize.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 from torch import Tensor
 
+from src.model.basic_block import norm_mean_std
+
 BG_CHANNEL_MEAN = -31.65
 BG_CHANNEL_STD = 12.66
 
@@ -23,3 +25,54 @@ class BgConstantNormalizer(nn.Module):
         spec = (spec - self.mean) / self.std
 
         return spec
+
+
+class BgInstanceNormalizer(nn.Module):
+    """
+    F, Tの次元の統計を使ってmean-std normalizationする
+    """
+
+    def __init__(self, eps: float = 1e-4):
+        super().__init__()
+        self.eps = eps
+
+    def forward(self, spec: Tensor) -> Tensor:
+        """
+        spec: (B, C, F, T)
+        """
+
+        return norm_mean_std(spec, (2, 3), self.eps)
+
+
+class BgLayerNormalizer(nn.Module):
+    """
+    C, F, Tの次元の統計を使ってmean-std normalizationする
+    """
+
+    def __init__(self, eps: float = 1e-4):
+        super().__init__()
+        self.eps = eps
+
+    def forward(self, spec: Tensor) -> Tensor:
+        """
+        spec: (B, C, F, T)
+        """
+
+        return norm_mean_std(spec, (1, 2, 3), self.eps)
+
+
+class BgBatchNormalizer(nn.Module):
+    """
+    B, F, Tの次元の統計を使ってmean-std normalizationする
+    """
+
+    def __init__(self, eps: float = 1e-4):
+        super().__init__()
+        self.eps = eps
+
+    def forward(self, spec: Tensor) -> Tensor:
+        """
+        spec: (B, C, F, T)
+        """
+
+        return norm_mean_std(spec, (0, 2, 3), self.eps)
