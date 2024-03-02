3435fd02b1281237e74fedfa94d9420dc6f44aef
bilzard <nakamine11@gmail.com>
2024-03-02 14:23:28 +0900

    channelのqualityを加味したfeature processorを実装

--- src/model/feature_processor/__init__.py
+++ src/model/feature_processor/__init__.py
@@ -1,9 +1,11 @@
 from .base import IdentityFeatureProcessor
 from .dual_feature_processor import DualFeatureProcessor
 from .dual_feature_processor_with_z_sim import DualFeatureProcessorWithZSim
+from .dual_spec_with_mask import DualFeatureProcessorWithMask
 
 __all__ = [
     "DualFeatureProcessor",
     "IdentityFeatureProcessor",
     "DualFeatureProcessorWithZSim",
+    "DualFeatureProcessorWithMask",
 ]
