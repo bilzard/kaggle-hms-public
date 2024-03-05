8334991ef18be05eb0a5488a35f4db91e081288d
bilzard <nakamine11@gmail.com>
2024-03-05 23:59:19 +0900

    implement efficientnet1d

--- src/model/eeg_encoder/__init__.py
+++ src/model/eeg_encoder/__init__.py
@@ -1,5 +1,6 @@
 from .eeg_net import EegNet
+from .efficientnet1d import EfficientNet1d
 from .resnet1d import ResNet1d
 from .resnet1d_v2 import ResNet1dV2
 
-__all__ = ["ResNet1d", "ResNet1dV2", "EegNet"]
+__all__ = ["ResNet1d", "ResNet1dV2", "EegNet", "EfficientNet1d"]
