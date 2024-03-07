787b67df0141ec196f07e22d1bd74bb6210fbb04
bilzard <nakamine11@gmail.com>
2024-03-07 18:57:04 +0900

    リソース名変更: V2 -> TwoStage

--- src/model/feature_processor/__init__.py
+++ src/model/feature_processor/__init__.py
@@ -1,6 +1,6 @@
 from .base import IdentityFeatureProcessor
 from .dual_feature_processor import DualFeatureProcessor
-from .dual_feature_processor_v2 import DualFeatureProcessorV2
+from .dual_feature_processor_two_stage import DualFeatureProcessorTwoStage
 from .dual_feature_processor_with_z_sim import DualFeatureProcessorWithZSim
 from .dual_spec_with_mask import DualFeatureProcessorWithMask
 
@@ -9,5 +9,5 @@ __all__ = [
     "IdentityFeatureProcessor",
     "DualFeatureProcessorWithZSim",
     "DualFeatureProcessorWithMask",
-    "DualFeatureProcessorV2",
+    "DualFeatureProcessorTwoStage",
 ]
