d57618dec0d628924b369cc1e0cdd717dfbc928f
bilzard <nakamine11@gmail.com>
2024-03-07 15:12:02 +0900

    1d model用のheadをheadモジュールに移動

--- src/model/head/__init__.py
+++ src/model/head/__init__.py
@@ -3,6 +3,7 @@ from .label_aligned_head_v2 import LabelAlignedHeadV2
 from .label_aligned_head_v2b import LabelAlignedHeadV2b
 from .label_aligned_head_v3 import LabelAlignedHeadV3
 from .label_aligned_head_v4 import LabelAlignedHeadV4
+from .mlp_head import Head
 from .simple_head import SimpleHead
 from .simple_head_v2 import SimpleHeadV2
 
@@ -14,4 +15,5 @@ __all__ = [
     "LabelAlignedHeadV2b",
     "LabelAlignedHeadV3",
     "LabelAlignedHeadV4",
+    "Head",
 ]
