e6579bcfe1a7129f919d39f306cb21e59875b30d
bilzard <nakamine11@gmail.com>
2024-03-08 12:33:35 +0900

    implement compose & mu-law encoding adapter

--- src/model/eeg_adapter/__init__.py
+++ src/model/eeg_adapter/__init__.py
@@ -1,3 +1,10 @@
 from .collate import EegDualPerChannelCollator, EegDualStackingCollator
+from .compose import Compose
+from .convert import MuLawEncoding
 
-__all__ = ["EegDualPerChannelCollator", "EegDualStackingCollator"]
+__all__ = [
+    "EegDualPerChannelCollator",
+    "EegDualStackingCollator",
+    "Compose",
+    "MuLawEncoding",
+]
---
+++ src/model/eeg_adapter/compose.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+
+class Compose(nn.Module):
+    def __init__(self, targets: list[nn.Module]):
+        super().__init__()
+        self.targets = targets
+
+    def __repl__(self) -> str:
+        adapter_classes = [adapter.__class__.__name__ for adapter in self.targets]
+        adapter_classes = ", ".join(adapter_classes)
+        return f"{self.__class__.__name__}(adapters={adapter_classes})"
+
+    @torch.no_grad()
+    def forward(self, eeg: Tensor, eeg_mask: Tensor) -> tuple[Tensor, Tensor]:
+        for adapter in self.targets:
+            eeg, eeg_mask = adapter(eeg, eeg_mask)
+
+        return eeg, eeg_mask
