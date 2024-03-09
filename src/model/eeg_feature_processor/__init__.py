8749533db257debfdc2f7e7d8831f16e7aa10864
bilzard <nakamine11@gmail.com>
2024-03-09 15:26:34 +0900

    GeM poolingによる1dモデル用のfeature processorを実装

--- src/model/eeg_feature_processor/__init__.py
+++ src/model/eeg_feature_processor/__init__.py
@@ -1,9 +1,11 @@
 from .dual_per_channel import EegDualPerChannelFeatureProcessor
 from .dual_per_channel_simple import EegDualPerChannelSimpleFeatureProcessor
+from .dual_per_channel_v2 import EegDualPerChannelFeatureProcessorV2
 from .dual_simple import EegDualSimpleFeatureProcessor
 
 __all__ = [
     "EegDualPerChannelFeatureProcessor",
     "EegDualPerChannelSimpleFeatureProcessor",
     "EegDualSimpleFeatureProcessor",
+    "EegDualPerChannelFeatureProcessorV2",
 ]
