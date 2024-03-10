c2d6b948d11762c1424adae0a3ef5ce5ef918a38
bilzard <nakamine11@gmail.com>
2024-03-10 16:28:45 +0900

    ensemble: evaluateの処理を関数に抽出

--- run/ensemble.py
+++ run/ensemble.py
@@ -13,6 +13,76 @@ from src.infer_util import load_metadata, make_submission
 from src.proc_util import trace
 
 
+def do_evaluate(metadata, predictions):
+    eval_df = metadata.join(predictions, on="eeg_id")
+
+    gts = eval_df.select(f"{label}_prob_per_eeg" for label in LABELS).to_numpy()
+    preds = eval_df.select(f"{label}_vote" for label in LABELS).to_numpy()
+    weights = eval_df["weight"].to_numpy()
+
+    gts = torch.from_numpy(gts.copy())
+    preds = torch.from_numpy(preds.copy())
+    weights = torch.from_numpy(weights.copy())
+    eeg_ids = eval_df["eeg_id"].to_list()
+
+    with torch.no_grad():
+        losses = kl_div(
+            log_softmax(preds, dim=1),
+            gts,
+            reduction="none",
+        )
+        loss_per_label = (
+            ((losses * weights[:, np.newaxis]).sum(dim=0) / weights.sum())
+            .detach()
+            .cpu()
+            .numpy()
+        )
+
+    # for error analysis
+    losses = losses.detach().cpu().numpy()
+    preds = preds.detach().cpu().numpy()
+    gts = gts.detach().cpu().numpy()
+    weights = weights.detach().cpu().numpy()
+
+    loss = loss_per_label.sum()
+    loss_per_label = dict(zip(LABELS, loss_per_label.tolist()))
+    print(f"loss: {loss:.4f}")
+    print(
+        "loss_per_label:",
+        ", ".join([f"{k}={v:.4f}" for k, v in loss_per_label.items()]),
+    )
+    print("losses:", losses.shape)
+    np.save("losses.npy", losses.astype(np.float16))
+    np.save("eeg_ids.npy", eeg_ids)
+    np.save("gts.npy", gts.astype(np.float16))
+    np.save("preds.npy", preds.astype(np.float16))
+    np.save("weights.npy", weights.astype(np.float16))
+
+    figure_path = Path("figure")
+    if not figure_path.exists():
+        figure_path.mkdir()
+
+    plot_loss_distribution(
+        preds,
+        gts,
+        weights,
+        save_file=True,
+        apply_softmax=True,
+        root_path=figure_path,
+    )
+    plot_confusion_matrix(
+        preds,
+        gts,
+        weights,
+        save_file=True,
+        apply_softmax=True,
+        root_path=figure_path,
+    )
+    losses = losses.sum(axis=1)
+
+    pl.DataFrame(dict(eeg_id=eeg_ids, loss=losses)).write_parquet("losses.pqt")
+
+
 @hydra.main(config_path="conf", config_name="ensemble", version_base="1.2")
 def main(cfg: EnsembleMainConfig):
     working_dir = Path(cfg.env.working_dir)
@@ -56,77 +126,7 @@ def main(cfg: EnsembleMainConfig):
                     group_by_eeg=True,
                     weight_key="weight_per_eeg",
                 )
-                eval_df = metadata.join(predictions, on="eeg_id")
-
-                gts = eval_df.select(
-                    f"{label}_prob_per_eeg" for label in LABELS
-                ).to_numpy()
-                preds = eval_df.select(f"{label}_vote" for label in LABELS).to_numpy()
-                weights = eval_df["weight"].to_numpy()
-
-                gts = torch.from_numpy(gts.copy())
-                preds = torch.from_numpy(preds.copy())
-                weights = torch.from_numpy(weights.copy())
-                eeg_ids = eval_df["eeg_id"].to_list()
-
-                with torch.no_grad():
-                    losses = kl_div(
-                        log_softmax(preds, dim=1),
-                        gts,
-                        reduction="none",
-                    )
-                    loss_per_label = (
-                        ((losses * weights[:, np.newaxis]).sum(dim=0) / weights.sum())
-                        .detach()
-                        .cpu()
-                        .numpy()
-                    )
-
-                # for error analysis
-                losses = losses.detach().cpu().numpy()
-                preds = preds.detach().cpu().numpy()
-                gts = gts.detach().cpu().numpy()
-                weights = weights.detach().cpu().numpy()
-
-                loss = loss_per_label.sum()
-                loss_per_label = dict(zip(LABELS, loss_per_label.tolist()))
-                print(f"loss: {loss:.4f}")
-                print(
-                    "loss_per_label:",
-                    ", ".join([f"{k}={v:.4f}" for k, v in loss_per_label.items()]),
-                )
-                print("losses:", losses.shape)
-                np.save("losses.npy", losses.astype(np.float16))
-                np.save("eeg_ids.npy", eeg_ids)
-                np.save("gts.npy", gts.astype(np.float16))
-                np.save("preds.npy", preds.astype(np.float16))
-                np.save("weights.npy", weights.astype(np.float16))
-
-                figure_path = Path("figure")
-                if not figure_path.exists():
-                    figure_path.mkdir()
-
-                plot_loss_distribution(
-                    preds,
-                    gts,
-                    weights,
-                    save_file=True,
-                    apply_softmax=True,
-                    root_path=figure_path,
-                )
-                plot_confusion_matrix(
-                    preds,
-                    gts,
-                    weights,
-                    save_file=True,
-                    apply_softmax=True,
-                    root_path=figure_path,
-                )
-                losses = losses.sum(axis=1)
-
-                pl.DataFrame(dict(eeg_id=eeg_ids, loss=losses)).write_parquet(
-                    "losses.pqt"
-                )
+                do_evaluate(metadata, predictions)
 
             case "test":
                 submission_df = make_submission(
