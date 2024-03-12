959d3b69ca3f943f78ab761505a3b64a9d7bb4f6
bilzard <nakamine11@gmail.com>
2024-03-12 23:36:16 +0900

    specのchannel dropを実装

--- src/model/augmentation/__init__.py
+++ src/model/augmentation/__init__.py
@@ -1,5 +1,6 @@
 from .base import Compose, Identity
+from .channel_drop import ChannelDrop
 from .cutmix_same_class import CutMixSameClassForSpec
 from .mixup import Mixup
 
-__all__ = ["Identity", "Compose", "CutMixSameClassForSpec", "Mixup"]
+__all__ = ["Identity", "Compose", "CutMixSameClassForSpec", "Mixup", "ChannelDrop"]
