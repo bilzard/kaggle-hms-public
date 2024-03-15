88cf37d4be3852c121c58d1edc508192683389cb
bilzard <nakamine11@gmail.com>
2024-03-15 18:17:11 +0900

    wavegramを挿げ替え可能に

--- src/model/feature_extractor/__init__.py
+++ src/model/feature_extractor/__init__.py
@@ -1,5 +1,11 @@
 from .eeg import ChannelCollator
 from .wave2spec import Wave2Spectrogram
 from .wave2wavegram import Wave2Wavegram
+from .wavegram import Wavegram
 
-__all__ = ["ChannelCollator", "Wave2Spectrogram", "Wave2Wavegram"]
+__all__ = [
+    "ChannelCollator",
+    "Wave2Spectrogram",
+    "Wave2Wavegram",
+    "Wavegram",
+]
