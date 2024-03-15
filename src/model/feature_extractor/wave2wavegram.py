0bad5c461dd00fea0fb9179600715f06eb2e3fbf
bilzard <nakamine11@gmail.com>
2024-03-15 17:21:08 +0900

    Wavegramによる2dモデルを実装

---
+++ src/model/feature_extractor/wave2wavegram.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+from einops import rearrange
+
+from src.model.feature_extractor import ChannelCollator
+from src.model.feature_extractor.wavegram import Wavegram
+from src.model.tensor_util import rolling_mean, same_padding_1d
+
+
+class Wave2Wavegram(nn.Module):
+    """
+    Wavegram[1]によるfilter bankの抽出
+
+    Reference:
+    ----------
+    [1] PANNs: Large-Scale Pretrained Audio Neural Networks for Audio Pattern Recognition
+    """
+
+    def __init__(
+        self,
+        sampling_rate: int = 40,
+        num_filter_banks: int = 64,
+        win_length: int = 64,
+        cutoff_freqs: tuple[float, float] = (0.5, 50),
+        stem_kernel_size: int = 3,
+        stem_stride: int = 1,
+        hidden_dims: list[int] = [64, 64, 64, 128, 128],
+        apply_mask: bool = True,
+        downsample_mode: str = "linear",
+        expand_mask: bool = True,
+    ):
+        super().__init__()
+        self.sampling_rate = sampling_rate
+        self.num_filter_banks = num_filter_banks
+        self.win_length = win_length
+        self.cutoff_freqs = cutoff_freqs
+        self.apply_mask = apply_mask
+        self.downsample_mode = downsample_mode
+        self.expand_mask = expand_mask
+        self.hop_length = stem_stride * 2 ** (len(hidden_dims) - 1)
+
+        self.collate_channels = ChannelCollator(
+            sampling_rate=sampling_rate,
+            cutoff_freqs=cutoff_freqs,
+            apply_mask=apply_mask,
+        )
+        self.wavegram = Wavegram(
+            in_channels=1,
+            out_channels=1,
+            stem_kernel_size=stem_kernel_size,
+            stem_stride=stem_stride,
+            hidden_dims=hidden_dims,
+            num_filter_banks=num_filter_banks,
+        )
+
+    def downsample_mask(self, x: torch.Tensor, mode="nearest") -> torch.Tensor:
+        """
+        1次元信号をダウンサンプリングする
+        """
+        x = same_padding_1d(
+            x,
+            kernel_size=self.win_length,
+            stride=self.hop_length,
+            mode="replicate",
+        )
+        if mode == "nearest":
+            x = x[..., self.win_length // 2 :: self.hop_length]
+        elif mode == "linear":
+            x = rolling_mean(
+                x,
+                kernel_size=self.win_length,
+                stride=self.hop_length,
+                apply_padding=False,
+            )
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
+
+        return x
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor | None = None):
+        """
+        return:
+        - signal: (B, C, T)
+        - spectrogram: (B, C, F, T)
+        - channel_mask: (B, C, T)
+        - spec_mask: (B, C, 1, T)
+        """
+        output = self.collate_channels(x, mask)
+        eeg = output["eeg"]
+        eeg_mask = output["eeg_mask"]
+
+        if mask is None:
+            mask = torch.ones_like(x)
+
+        spec_mask = self.downsample_mask(eeg_mask, mode=self.downsample_mode)
+        spec_mask = spec_mask.unsqueeze(dim=2)
+
+        B, Ch, T = eeg.shape
+        eeg = rearrange(eeg, "b ch t -> (b ch) 1 t")
+        spec = self.wavegram(eeg)
+        spec = rearrange(spec, "(b ch) 1 f t -> b ch f t", b=B, ch=Ch)
+        B, C, F, T = spec.shape
+        spec_mask = spec_mask[..., :T]
+        BM, CM, FM, TM = spec_mask.shape
+        assert B == BM and C == CM and T == TM, (spec.shape, spec_mask.shape)
+
+        if self.expand_mask:
+            F = spec.shape[2]
+            spec_mask = spec_mask.expand(-1, -1, F, -1)
+
+        output = dict(spec=spec, eeg=eeg, eeg_mask=eeg_mask, spec_mask=spec_mask)
+        return output
+
+
+if __name__ == "__main__":
+    from torchinfo import summary
+
+    batch_size = 2
+    num_frames = 2048
+    num_probes = 19
+    num_channels = num_probes - 1
+    num_filter_banks = 64
+    hidden_dims = [64, 64, 64, 128, 128]
+    win_length = 64
+
+    eeg = torch.randn(batch_size, num_frames, num_probes)
+    mask = torch.rand(batch_size, num_frames, num_probes)
+    model = Wave2Wavegram(
+        hidden_dims=hidden_dims,
+        num_filter_banks=num_filter_banks,
+        win_length=win_length,
+    )
+    output = model(eeg, mask)
+    assert output["spec"].shape == (
+        batch_size,
+        num_channels,
+        num_filter_banks,
+        num_frames // 2 ** (len(hidden_dims) - 1),
+    ), output["spec"].shape
+
+    summary(model, input_size=(batch_size, num_frames, num_probes))
