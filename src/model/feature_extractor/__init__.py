259b5d44cc0327db337db59ecfb6ec1d3a6aac07
bilzard <nakamine11@gmail.com>
2024-03-15 17:21:08 +0900

    Wavegramによる2dモデルを実装

--- src/model/feature_extractor/__init__.py
+++ src/model/feature_extractor/__init__.py
@@ -1,4 +1,5 @@
 from .eeg import ChannelCollator
 from .wave2spec import Wave2Spectrogram
+from .wave2wavegram import Wave2Wavegram
 
-__all__ = ["ChannelCollator", "Wave2Spectrogram"]
+__all__ = ["ChannelCollator", "Wave2Spectrogram", "Wave2Wavegram"]
