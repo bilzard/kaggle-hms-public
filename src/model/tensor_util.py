9142cd05b515abf9654345b876d56b74980d1598
bilzard <nakamine11@gmail.com>
2024-03-15 14:34:37 +0900

    rolling_meanの処理を簡潔化

--- src/model/tensor_util.py
+++ src/model/tensor_util.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 def same_padding_1d(
@@ -16,28 +17,32 @@ def rolling_mean(
     kernel_size: int,
     stride: int,
     apply_padding: bool = True,
-    no_grad: bool = True,
 ) -> torch.Tensor:
     """
     x: (B, C, T)
     """
-    B, C, T = x.shape
+    _, C, _ = x.shape
     pad_size = (kernel_size - stride) // 2 if apply_padding else 0
 
-    conv = nn.Conv1d(
-        in_channels=C,
-        out_channels=C,
-        kernel_size=kernel_size,
-        stride=stride,
-        padding=pad_size,
-        bias=False,
-        groups=C,
+    weight = torch.full(
+        (C, 1, kernel_size),
+        fill_value=1.0 / kernel_size,
+        device=x.device,
+        dtype=x.dtype,
     )
-    conv = conv.to(x.device)
-    conv.weight.data.fill_(1.0 / kernel_size)
-    if no_grad:
-        with torch.no_grad():
-            result = conv(x)
-    else:
-        result = conv(x)
+    result = F.conv1d(x, weight, stride=stride, padding=pad_size, groups=C)
     return result
+
+
+if __name__ == "__main__":
+    x = torch.arange(10).reshape(1, 1, -1).float()
+    print("x:", x.detach().numpy().flatten())
+    print("same_padding_1d", same_padding_1d(x, 3, 1).detach().numpy())
+    print(
+        "rolling_mean(k=3, s=1)",
+        rolling_mean(
+            same_padding_1d(x, 3, 1, mode="replicate"), 3, 1, apply_padding=False
+        )
+        .detach()
+        .numpy(),
+    )
