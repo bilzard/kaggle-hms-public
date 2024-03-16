706c58a88bc7bdec4242680f677cdd0980c177e0
bilzard <nakamine11@gmail.com>
2024-03-16 10:15:30 +0900

    schedulerで開始ステップを指定可能に

--- src/scheduler/linear.py
+++ src/scheduler/linear.py
@@ -4,29 +4,45 @@ class LinearScheduler:
     (0, initial_value) -> (target_step, target_value)
     """
 
-    def __init__(self, initial_value, target_value, target_step):
+    def __init__(
+        self,
+        target_step: int,
+        initial_value: float,
+        target_value: float,
+        schedule_start_step: int = 0,
+    ):
+        assert (
+            schedule_start_step <= target_step
+        ), "schedule_start_step should be less than or equal to target_step"
         self.current_step = 0
-        self._current_value = initial_value
+        self.initial_step = schedule_start_step
+        self.current_value = initial_value
         self.target_value = target_value
         self.target_step = target_step
 
-        if self.target_step > 1:
-            self.delta = (target_value - initial_value) / target_step
+        if self.target_step - self.initial_step > 1:
+            self.delta = (target_value - initial_value) / (
+                target_step - schedule_start_step
+            )
         else:
             self.delta = 0
 
     def step(self):
-        if self.current_step < self.target_step:
-            self._current_value += self.delta
+        if self.initial_step <= self.current_step < self.target_step:
+            self.current_value += self.delta
+        elif self.current_step == self.target_step:
+            self.current_value = self.target_value
         self.current_step += 1
 
     @property
     def value(self):
-        return self._current_value
+        return self.current_value
 
 
 if __name__ == "__main__":
-    scheduler = LinearScheduler(initial_value=1.0, target_value=0.5, target_step=10)
+    scheduler = LinearScheduler(
+        schedule_start_step=5, initial_value=1.0, target_value=0.5, target_step=10
+    )
 
     for epoch in range(15):
         print(f"Epoch {epoch}, Value: {scheduler.value:.4f}")
