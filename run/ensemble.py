b97de05c538ba8aefa773c3d701b3f886b87bf9e
bilzard <nakamine11@gmail.com>
2024-03-23 16:27:49 +0900

    cvスコアの計算をcpuで行う

--- run/ensemble.py
+++ run/ensemble.py
@@ -3,84 +3,42 @@ from pathlib import Path
 import hydra
 import numpy as np
 import polars as pl
-import torch
-from torch.nn.functional import kl_div, log_softmax
+from scipy.special import kl_div, softmax
 
 from src.config import EnsembleMainConfig
 from src.constant import LABELS
-from src.error_analysis import plot_confusion_matrix, plot_loss_distribution
 from src.infer_util import load_metadata, make_submission
 from src.proc_util import trace
 
 
-def do_evaluate(metadata, predictions):
+def do_evaluate(
+    metadata: pl.DataFrame, predictions: pl.DataFrame, verbose: bool = True
+):
     eval_df = metadata.join(predictions, on="eeg_id")
 
     gts = eval_df.select(f"{label}_prob_per_eeg" for label in LABELS).to_numpy()
     preds = eval_df.select(f"{label}_vote" for label in LABELS).to_numpy()
     weights = eval_df["weight"].to_numpy()
-
-    gts = torch.from_numpy(gts.copy())
-    preds = torch.from_numpy(preds.copy())
-    weights = torch.from_numpy(weights.copy())
     eeg_ids = eval_df["eeg_id"].to_list()
 
-    with torch.no_grad():
-        losses = kl_div(
-            log_softmax(preds, dim=1),
-            gts,
-            reduction="none",
-        )
-        loss_per_label = (
-            ((losses * weights[:, np.newaxis]).sum(dim=0) / weights.sum())
-            .detach()
-            .cpu()
-            .numpy()
-        )
-
-    # for error analysis
-    losses = losses.detach().cpu().numpy()
-    preds = preds.detach().cpu().numpy()
-    gts = gts.detach().cpu().numpy()
-    weights = weights.detach().cpu().numpy()
+    losses = kl_div(gts, softmax(preds, axis=1))
+    loss_per_label = (losses * weights[:, np.newaxis]).sum(axis=0) / weights.sum()
 
     loss = loss_per_label.sum()
     loss_per_label = dict(zip(LABELS, loss_per_label.tolist()))
-    print(f"loss: {loss:.4f}")
-    print(
-        "loss_per_label:",
-        ", ".join([f"{k}={v:.4f}" for k, v in loss_per_label.items()]),
-    )
-    print("losses:", losses.shape)
-    np.save("losses.npy", losses.astype(np.float16))
-    np.save("eeg_ids.npy", eeg_ids)
-    np.save("gts.npy", gts.astype(np.float16))
-    np.save("preds.npy", preds.astype(np.float16))
-    np.save("weights.npy", weights.astype(np.float16))
 
-    figure_path = Path("figure")
-    if not figure_path.exists():
-        figure_path.mkdir()
+    if verbose:
+        print(f"* loss: {loss:.4f}")
+        print(
+            "* loss_per_label:",
+            ", ".join([f"{k}={v:.4f}" for k, v in loss_per_label.items()]),
+        )
+        print("* losses:", losses.shape)
 
-    plot_loss_distribution(
-        preds,
-        gts,
-        weights,
-        save_file=True,
-        apply_softmax=True,
-        root_path=figure_path,
-    )
-    plot_confusion_matrix(
-        preds,
-        gts,
-        weights,
-        save_file=True,
-        apply_softmax=True,
-        root_path=figure_path,
-    )
     losses = losses.sum(axis=1)
 
-    pl.DataFrame(dict(eeg_id=eeg_ids, loss=losses)).write_parquet("losses.pqt")
+    loss_df = pl.DataFrame(dict(eeg_id=eeg_ids, loss=losses))
+    return loss_df
 
 
 @hydra.main(config_path="conf", config_name="ensemble", version_base="1.2")
@@ -93,6 +51,7 @@ def main(cfg: EnsembleMainConfig):
 
     with trace("load predictions"):
         print(f"ensemble_name: {ensemble_entity.name}")
+        num_seeds = 0
         predictions = []
         for experiment in ensemble_entity.experiments:
             local_preds = []
@@ -108,11 +67,16 @@ def main(cfg: EnsembleMainConfig):
                             / f"pred_{cfg.phase}.pqt"
                         )
                     )
+                    num_seeds += 1
             local_preds = pl.concat(local_preds)
             local_preds = local_preds.group_by("eeg_id", maintain_order=True).agg(
                 pl.col(f"{label}_vote").mean() for label in LABELS
             )
             predictions.append(local_preds)
+
+        print(
+            f"* ensemble of {len(ensemble_entity.experiments)} models (total seeds={num_seeds})"
+        )
 
         predictions = pl.concat(predictions)
         predictions = predictions.group_by("eeg_id", maintain_order=True).agg(
@@ -129,7 +93,8 @@ def main(cfg: EnsembleMainConfig):
                     weight_key="weight_per_eeg",
                     num_samples=cfg.dev.num_samples,
                 )
-                do_evaluate(metadata, predictions)
+                loss_df = do_evaluate(metadata, predictions)
+                loss_df.write_parquet("losses.pqt")
 
             case "test":
                 submission_df = make_submission(
