2422679abbdb7792a9a42c6216a5b72681681461
bilzard <nakamine11@gmail.com>
2024-03-23 18:24:34 +0900

    Inverted Residualベースのwavegaramを廃止(学習が難しく発散する)

--- src/model/feature_extractor/module/__init__.py
+++ src/model/feature_extractor/module/__init__.py
@@ -1,5 +1,4 @@
-from .efficient_wavegram import EfficientWavegram
 from .melspec import MelSpec
 from .wavegram import Wavegram
 
-__all__ = ["EfficientWavegram", "Wavegram", "MelSpec"]
+__all__ = ["Wavegram", "MelSpec"]
