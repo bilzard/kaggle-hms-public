c52e115bef7b63f27bebc3ac0e6a1ab656294a2f
bilzard <nakamine11@gmail.com>
2024-03-25 21:43:55 +0900

    EEGのTimeCropping Transformを実装

--- src/model/eeg_adapter/__init__.py
+++ src/model/eeg_adapter/__init__.py
@@ -1,10 +1,12 @@
 from .collate import EegDualPerChannelCollator, EegDualStackingCollator
 from .compose import Compose
 from .convert import MuLawEncoding
+from .transform import EegTimeCroppingTransform
 
 __all__ = [
     "EegDualPerChannelCollator",
     "EegDualStackingCollator",
     "Compose",
     "MuLawEncoding",
+    "EegTimeCroppingTransform",
 ]
