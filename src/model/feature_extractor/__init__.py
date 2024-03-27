f3c89163cd7a95831bff4a7e418647e7088e96f2
bilzard <nakamine11@gmail.com>
2024-03-27 17:35:25 +0900

    ChannelCollatorの定義階層を一段深く

--- src/model/feature_extractor/__init__.py
+++ src/model/feature_extractor/__init__.py
@@ -1,4 +1,4 @@
-from .eeg import ChannelCollator
+from .collator import ChannelCollator
 from .wave2panns import Wave2Panns
 from .wave2spec import Wave2Spectrogram
 from .wave2wavegram import Wave2Wavegram
