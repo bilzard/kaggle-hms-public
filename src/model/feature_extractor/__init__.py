9b9e75d8285bd683efb8ce03034cff4dd41aa0e6
bilzard <nakamine11@gmail.com>
2024-04-02 20:28:36 +0900

    任意のprobe groupを指定可能に

--- src/model/feature_extractor/__init__.py
+++ src/model/feature_extractor/__init__.py
@@ -3,4 +3,9 @@ from .wave2panns import Wave2Panns
 from .wave2spec import Wave2Spectrogram
 from .wave2wavegram import Wave2Wavegram
 
-__all__ = ["ChannelCollator", "Wave2Spectrogram", "Wave2Wavegram", "Wave2Panns"]
+__all__ = [
+    "ChannelCollator",
+    "Wave2Spectrogram",
+    "Wave2Wavegram",
+    "Wave2Panns",
+]
